c3684c99c75dde81a5176c021c391c683d58fa66
KrzysztofKuKliński <zono.mrzono21@gmail.com>
2025-12-18 18:58:15 +0100

    fix(follow): Add missing imports to FollowController
    Fixed the `FollowController` by adding missing `use` statements.
    * The code was crashing because `User`, `EntityManager`, `Request`, and `IsGranted` were not imported.

--- symfony/src/Controller/FollowController.php
+++ symfony/src/Controller/FollowController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/follow')]
 #[IsGranted('ROLE_USER')]
