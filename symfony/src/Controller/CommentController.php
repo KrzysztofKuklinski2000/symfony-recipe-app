418424f94f4c6a43eca82149f8d7b57de741caf9
KrzysztofKuKliński <zono.mrzono21@gmail.com>
2026-01-06 20:23:00 +0100

    fix(comment): repair controller logic and turbo stream
    * Added  User import and moved dependencies to the constructor.
    * Corrected typo in notifier variable name.
    * Safer redirection after deleting a comment.

--- symfony/src/Controller/CommentController.php
+++ symfony/src/Controller/CommentController.php
@@ -2,45 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\Recipe;
 use App\Entity\Comment;
-use App\Form\CommentType;
 
+use App\Form\CommentType;
+use Symfony\UX\Turbo\TurboBundle;
 use App\Security\Voter\CommentVoter;
+use App\Notifier\CommentNotification;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\UX\Turbo\TurboBundle;
-use App\Notifier\CommentNotification;
 use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Notifier\Recipient\Recipient;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/comment')]
 #[IsGranted('ROLE_USER')]
 #[IsGranted('IS_EMAIL_VERIFIED')]
 final class CommentController extends AbstractController
 {
+    public function __construct(
+        private readonly NotifierInterface $notifier,
+        private readonly EntityManagerInterface $em
+    ){}
+
     #[Route('/add/{id}', name: 'app_comment_add', methods: ['POST'])]
-    public function add(Recipe $recipe, EntityManagerInterface $em, Request $request, NotifierInterface $notifer): Response
+    public function add(Recipe $recipe, Request $request): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentType::class, $comment);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $comment->setAuthor($this->getUser());
+            $user = $this->getUser();
+            assert($user instanceof User);
+
+            $comment->setAuthor($user);
             $comment->setRecipe($recipe);
 
-            $em->persist($comment);
-            $em->flush();
+            $this->em->persist($comment);
+            $this->em->flush();
 
-            if($recipe->getAuthor() !== $this->getUser()){
+            if($recipe->getAuthor() !== $user){
                 $recipient = new Recipient($recipe->getAuthor()->getEmail());
 
-                $notifer->send(
+                $this->notifier->send(
                     new CommentNotification($comment, $recipe),
                     $recipient
                 );
@@ -71,26 +80,30 @@ final class CommentController extends AbstractController
     }
 
     #[Route('/delete/{id}', name: 'app_comment_delete', methods: ['POST'])]
-    public function delete(Comment $comment, EntityManagerInterface $em, Request $request): Response {
+    public function delete(Comment $comment, Request $request): Response {
 
         $this->denyAccessUnlessGranted(CommentVoter::DELETE, $comment);
 
-        $token = $request->request->get('_token');
+        $recipeId = $comment->getRecipe()->getId();
+        $recipe = $comment->getRecipe();
 
-        if($this->isCsrfTokenValid('delete'.$comment->getId(), $token)){
-            $em->remove($comment);
-            $em->flush();
+        if($this->isCsrfTokenValid('delete'.$comment->getId(), $request->request->get('_token'))){
+            $this->em->remove($comment);
+            $this->em->flush();
 
             if (TurboBundle::STREAM_FORMAT === $request->getPreferredFormat())
             {
                 $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
                 return $this->render('comment/delete_success.stream.html.twig', [
                     'comment' => $comment,
-                    'recipe' => $comment->getRecipe(),
+                    'recipe' => $recipe,
                 ]);
             }
         }
 
-        return $this->redirectToRoute('app_show', ['id' => $comment->getRecipe()->getId(), '_fragment'=>'comments']);
+        return $this->redirectToRoute('app_show', [
+            'id' => $recipeId,
+            '_fragment'=>'comments'
+        ]);
     }
 }
