7740d05a42d679e1c7e36cdecb35ec89b2f17e55
KrzysztofKuKliński <zono.mrzono21@gmail.com>
2026-01-07 17:22:57 +0100

    refactor(profile): cleanup profile controller
    * Moved RecipeRepository to the constructor.
    * Removed unused imports (EntityManager, Request, UserRepository).
    * Removed redundant variable in followingFeed method.

--- symfony/src/Controller/ProfileController.php
+++ symfony/src/Controller/ProfileController.php
@@ -3,10 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Repository\UserRepository;
 use App\Repository\RecipeRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -15,17 +12,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/profile')]
 final class ProfileController extends AbstractController
 {
+
+    public function __construct(private readonly RecipeRepository $recipeRepository) {}
+
     #[Route('/following', name: 'app_profile_index')]
     #[IsGranted('ROLE_USER')]
-    public function followingFeed(RecipeRepository $recipeRepository): Response {
-        /** @var User $currentUser*/
+    public function followingFeed(): Response {
         $currentUser = $this->getUser();
-        $users = $currentUser->getFollowing();
+        assert($currentUser instanceof User);
 
-        $recipes = $recipeRepository->findRecipesFromFollowing($currentUser);
+        $recipes = $this->recipeRepository->findRecipesFromFollowing($currentUser);
 
         return $this->render('profile/following_feed.html.twig', [
-            'users' => $users,
+            'users' => $currentUser->getFollowing(),
             'recipes' => $recipes,
         ]);
     }
