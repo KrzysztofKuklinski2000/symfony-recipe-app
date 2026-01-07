67deb70fa81e6ac09192b83b402baebf01960369
KrzysztofKuKliński <zono.mrzono21@gmail.com>
2026-01-07 18:43:21 +0100

    refactor(recipe): modernize recipe controller and fix delete bug
    * Injected `FileUploader` via constructor, fixing an "undefined variable" crash in the delete method.
    * Simplified image upload logic to use the updated service (automatic old file deletion).
    * Added try-catch blocks for file uploads.
    * Moved EntityManager to the constructor.

--- symfony/src/Controller/RecipeController.php
+++ symfony/src/Controller/RecipeController.php
@@ -2,51 +2,69 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\Recipe;
 use App\Form\RecipeType;
 use App\Service\FileUploader;
 use App\Security\Voter\RecipeVoter;
 use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormInterface;
 
 #[Route('/recipe')]
 #[IsGranted('IS_EMAIL_VERIFIED')]
 final class RecipeController extends AbstractController
 {
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+        private readonly FileUploader $fileUploader
+    ){}
+
     #[Route('/new', name: 'app_recipe_new', methods: ['GET', 'POST'])]
     #[IsGranted(RecipeVoter::CREATE)]
-    public function new(Request $request, EntityManagerInterface $em, FileUploader $fileUploader): Response {
+    public function new(Request $request): Response {
 
         $recipe = new Recipe();
         $form = $this->createForm(RecipeType::class, $recipe);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();
+            assert($user instanceof User);
 
-            $this->handleImageUpload($form, $recipe, $fileUploader);
+            $recipe->setAuthor($user);
 
-            $recipe->setAuthor($this->getUser());
-            $em->persist($recipe);
-            $em->flush();
+            try {
+                $this->handleImageUpload($form, $recipe);
+            } catch (FileException $e) {
+                $this->addFlash('danger', 'Nie udało się wgrać zdjęcia.');
+                return $this->render('recipe/new.html.twig', [
+                    'form' => $form,
+                    'recipe' => $recipe,
+                ]);
+            }
+
+
+            $this->em->persist($recipe);
+            $this->em->flush();
 
             $this->addFlash('success', 'Przepis został dodany!');
-            return $this->redirectToRoute('app_profile_show', ['id' => $this->getUser()->getId()]);
+            return $this->redirectToRoute('app_profile_show', ['id' => $user->getId()]);
         }
 
         return $this->render('recipe/new.html.twig', [
-            'form' => $form->createView(),
+            'form' => $form,
             'recipe' => $recipe,
         ]);
     }
 
     #[Route('/edit/{id}', name: 'app_recipe_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Recipe $recipe, EntityManagerInterface $em, FileUploader $fileUploader): Response {
+    public function edit(Request $request, Recipe $recipe): Response {
         $this->denyAccessUnlessGranted(RecipeVoter::EDIT, $recipe);
 
         $form = $this->createForm(RecipeType::class, $recipe);
@@ -54,36 +72,42 @@ final class RecipeController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $this->handleImageUpload($form, $recipe, $fileUploader);
+            try {
+                $this->handleImageUpload($form, $recipe);
+            } catch (FileException $e) {
+                $this->addFlash('danger', 'Nie udało się zaktualizować zdjęcia.');
+                return $this->redirectToRoute('app_recipe_edit', ['id' => $recipe->getId()]);
+            }
 
-            $em->flush();
+            $this->em->flush();
 
             $this->addFlash('success', 'Uaktualniono przepis!');
             return $this->redirectToRoute('app_show', ['id' => $recipe->getId()]);
         }
 
         return $this->render('recipe/edit.html.twig', [
-            'form' => $form->createView(),
+            'form' => $form,
             'recipe' => $recipe,
         ]);
     }
 
     #[Route('/delete/{id}', name: 'app_recipe_delete', methods: ['POST'])]
-    public function delete(Request $request, Recipe $recipe, EntityManagerInterface $em): Response {
+    public function delete(Request $request, Recipe $recipe): Response {
+
         $this->denyAccessUnlessGranted(RecipeVoter::DELETE, $recipe);
 
+        $user = $this->getUser();
+        assert($user instanceof User);
+        $userId = $user->getId();
 
-        $token = $request->request->get('_token');
-        $userId = $this->getUser()->getId();
-
-        if($this->isCsrfTokenValid('delete'.$recipe->getId(), $token)) {
+        if($this->isCsrfTokenValid('delete'.$recipe->getId(), $request->request->get('_token'))) {
             $filename = $recipe->getImageFilename();
 
-            $em->remove($recipe);
-            $em->flush();
+            $this->em->remove($recipe);
+            $this->em->flush();
 
             if($filename) {
-                $fileUploader->remove($filename, 'recipes');
+                $this->fileUploader->remove($filename, 'recipes');
             }
 
             $this->addFlash('success', 'Przepis został usunięty!');
@@ -93,19 +117,16 @@ final class RecipeController extends AbstractController
         return $this->redirectToRoute('app_profile_show', ['id' => $userId]);
     }
 
-    private function handleImageUpload(FormInterface $form, Recipe $recipe, FileUploader $fileUploader): void {
+    private function handleImageUpload(FormInterface $form, Recipe $recipe): void {
         $imageFile = $form->get('imageFile')->getData();
 
         if ($imageFile) {
-
-            $oldFilename = $recipe->getImageFilename();
-
-            $imageFilename = $fileUploader->upload($imageFile, 'recipes');
+            $imageFilename = $this->fileUploader->upload(
+                $imageFile,
+                'recipes',
+                $recipe->getImageFilename()
+            );
             $recipe->setImageFilename($imageFilename);
-
-            if($oldFilename) {
-                $fileUploader->remove($oldFilename, 'recipes');
-            }
         }
     }
 }
